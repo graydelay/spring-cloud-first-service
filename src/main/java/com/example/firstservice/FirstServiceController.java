b9b7d2a76238c99b17803fc06aaed3d12877beb8
delaylee <delaylee219@gmail.com>
2024-04-10 11:05:54 +0900

    feat: use random port

--- src/main/java/com/example/firstservice/FirstServiceController.java
+++ src/main/java/com/example/firstservice/FirstServiceController.java
@@ -1,6 +1,8 @@
 package com.example.firstservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +30,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/first-service")
 public class FirstServiceController {
 
+    Environment env;
+
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome first service application";
@@ -40,7 +48,10 @@ public class FirstServiceController {
     }
 
     @GetMapping("/check")
-    public String check() {
-        return "Hi, there. This is a message from First Service.";
+    public String check(HttpServletRequest request) {
+        log.info("Server port={}", request.getServerPort());
+
+        return String.format("Hi, there. This is a message from First Service on PORT %s"
+                , env.getProperty("local.server.port"));
     }
 }
